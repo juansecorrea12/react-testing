a62d971ab987fb6c0f1c735066e90317848d8ce7
juan correa <juancorrea@MacBook-Pro-de-juan.local>
2023-01-19 17:49:44 -0500

    changes part to objects

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import Total from './Total';
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  
   return (
     <div>
       <Header title = {course} />
       <Content 
-      espacio1 = {part1 + ' ' + exercises1}
-      espacio2 = {part2 + ' ' + exercises2} 
-      espacio3 = {part3 + ' ' + exercises3}
+      espacio1 = {part1['name'] + ' ' + part1['exercises']}
+      espacio2 = {part2.name + ' ' + part2.exercises}
+      espacio3 = {part3.name + ' ' + part3.exercises}
       />
-      <Total total = {'Number of exercises ' + (exercises1 + exercises2 + exercises3)}/>
+      <Total total = {'Number of exercises ' + (part1['exercises'] + part2['exercises'] + part3['exercises'])}/>
     </div>
   )
 }
